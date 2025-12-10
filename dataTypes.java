1c72e208899553850fb99d67d63886fd603ba1cd
abhi141Y <168571932+abhi141Y@users.noreply.github.com>
2025-12-10 15:24:56 +0530

    datatypes

---
+++ dataTypes.java
@@ -0,0 +1,14 @@
+class dataTypes{
+    public static void main(String args[]){
+        int myNum = 5;              
+float myFloatNum = 5.99f;    
+char myLetter = 'D';         
+boolean myBool = true;       
+String myText = "Hello";     
+System.out.println(myNum);
+System.out.println(myLetter);
+System.out.println(myBool);
+System.out.println(myFloatNum);
+System.out.println(myText);
+    }
+}
